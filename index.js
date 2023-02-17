aa34cb31e86b09faacd361c03d3e9cfd9c6d2dec
AldoRmzCode <aldoraga0209@gmail.com>
2023-02-17 17:55:11 -0600

    supermejorado01

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 //principal:
 app.use(express.static('public'));
-app.use(express.static('images'));
+app.use("/images", express.static("images"));
 
     
 app.listen(process.env.PORT || 3001, () => {
